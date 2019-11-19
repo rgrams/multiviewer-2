ce270ccd335016f343a5745197b2380a8668d2cc
rossgrams <ross.grams@gmail.com>
2019-11-19 09:10:38 -0500

    Conf.lua - Convert spaces to tabs.

--- conf.lua
+++ conf.lua
@@ -1,14 +1,14 @@
 
 function love.conf(t)
-    t.console = false
-    t.window.title = "Multiviewer 2.0 - No project"
-    t.window.icon = "Multiviewer_icon.png"
-    t.window.width = 800
-    t.window.height = 600
-    t.window.borderless = false
-    t.window.resizable = true
-    t.window.fullscreen = false
-    t.window.fullscreentype = "desktop"
-    t.window.vsync = 1
-    t.window.display = 1
+	t.console = false
+	t.window.title = "Multiviewer 2.0 - No project"
+	t.window.icon = "Multiviewer_icon.png"
+	t.window.width = 800
+	t.window.height = 600
+	t.window.borderless = false
+	t.window.resizable = true
+	t.window.fullscreen = false
+	t.window.fullscreentype = "desktop"
+	t.window.vsync = 1
+	t.window.display = 1
 end
